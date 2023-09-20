917d84f0bdb137eaed78d5e926e468b00a2b4409
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-09-20 09:16:28 +0200

    add ProductDetailDto

--- src/main/java/ch/csbe/backendlb/Product/DTO/ProductDetailDto.java
+++ src/main/java/ch/csbe/backendlb/Product/DTO/ProductDetailDto.java
@@ -1,4 +1,36 @@
 package ch.csbe.backendlb.Product.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+@Data
 public class ProductDetailDto {
+    @NotNull
+    @Schema(description = "einmalige Id eines Produktes zum identifiziren")
+    public Long id;
+    @NotNull
+    @Schema (description = "sku eines Produktes")
+    public String sku;
+    @NotNull
+    @Schema (description = "aktivitäts status eines Produktes")
+    public  String active;
+    @NotNull
+    @Schema (description = "Name eines Produktes")
+    public String name;
+    @NotNull
+    @Schema (description = "Das Bild zu einem Produkt")
+    public String images;
+    @NotNull
+    @Schema (description = "BEschreibung eines Produktes")
+    public String description;
+    @NotNull
+    @Schema (description = "Preis eines Produktes")
+    public Double prise;
+    @NotNull
+    @Schema (description = "Lagerbestand eines Produktes")
+    public String stock;
+    @NotNull
+    @Schema (description = "Kategory eines Produktes")
+    private CategoryShowDto toDos;
 }
+
