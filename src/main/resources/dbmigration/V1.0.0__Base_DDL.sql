fde9d0c82765ad97e1b4f1d12a74a14fbb64c9cf
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-09-20 13:30:03 +0200

    add file

---
+++ src/main/resources/dbmigration/V1.0.0__Base_DDL.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `todo` (
+                        `id` bigint(20) NOT NULL,
+                        `todo` varchar(255) NOT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
